f83e3a138ee954b99b89b15addda4ce746c090ed
ashutosz <ashutoshrai08022003@gmail.com>
2023-04-23 17:05:44 +0530

    All functions are added in app

--- lib/main.dart
+++ lib/main.dart
@@ -9,61 +9,83 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note1.wav'));
-                  },
-                  child: Text('click me'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note2.wav'));
-                  },
-                  child: Text('click me'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note3.wav'));
-                  },
-                  child: Text('click me'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note24.wav'));
-                  },
-                  child: Text('click me'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note5.wav'));
-                  },
-                  child: Text('click me'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note6.wav'));
-                  },
-                  child: Text('click me'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note7.wav'));
-                  },
-                  child: Text('click me'),
-                ),
-              ],
-            ),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red),),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note1.wav'));
+                },                  
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 10.0,
+              ),
+              TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orange),),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note2.wav'));
+                },                  
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 10.0,
+              ),
+              TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.yellow)),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note3.wav'));
+                },                  
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 10.0,
+              ),
+              TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note24.wav'));
+                },                  
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 10.0,
+              ),
+              TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.teal)),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note5.wav'));
+                },                  
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 10.0,
+              ),
+              TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note6.wav'));
+                },                  
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 10.0,
+              ),
+              TextButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.purple)),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note7.wav'));
+                },                  
+              ),
+            ],
           ),
         ),
       ),
