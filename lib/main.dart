13ac2c5682980449ca9844e3bc300cd752702219
ashutosz <ashutoshrai08022003@gmail.com>
2023-04-23 16:45:56 +0530

    All Seven Sound are added

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,59 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: TextButton(
-              onPressed: (){
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: Text('click me'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note1.wav'));
+                  },
+                  child: Text('click me'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  child: Text('click me'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note3.wav'));
+                  },
+                  child: Text('click me'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note24.wav'));
+                  },
+                  child: Text('click me'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  child: Text('click me'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  child: Text('click me'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  child: Text('click me'),
+                ),
+              ],
             ),
           ),
         ),
